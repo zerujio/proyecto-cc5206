2e73a9b067519864334211ac43f1c1eb4af326bb
Sergio Morales <semorales@ug.uchile.cl>
2021-09-30 13:50:02 -0300

    exploracion de datos

---
+++ get_datasets.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR_DATOS=datos
+DIR_MINEDUC=mineduc
+
+download_data()
+{
+    FOLDER=$DIR_DATOS/$3
+    FILENAME=$2
+    URL=$1
+
+    mkdir -p $FOLDER
+    wget -O $FOLDER/$FILENAME $URL
+}
+
+# Rendimiento escolar en Chile
+download_data http://datos.mineduc.cl/datasets/195396-resumen-rendimiento-ano-2020.download/ 2020.csv $DIR_MINEDUC/resumen_rendimiento
+
+# NEM
+download_data http://datos.mineduc.cl/datasets/195848-nem-y-percentil-jovenes-ano-2020.download/ jovenes_2020.csv $DIR_MINEDUC/NEM/
